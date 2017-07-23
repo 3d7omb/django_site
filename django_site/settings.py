6d224a1a83a850cdf9e79b9f876335ed676834dd
3d7omb <3d7omb@gmail.com>
2017-07-23 18:26:50 +0100

    testing

--- django_site/settings.py
+++ django_site/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -82,7 +84,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
@@ -119,8 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/'
 GOOGLE_RECAPTCHA_SECRET_KEY = '6Ld8ziYUAAAAAO0DRH2R1KYmX1X40IJjTwCzobwQ'
